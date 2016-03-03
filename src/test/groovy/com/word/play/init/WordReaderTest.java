8a7e0e3f8d6a570f68c76d6de1bef78c74042a4d
Ravi Vasamsetty <ravi.vasamsetty@outlook.com>
2016-03-02 23:13:06 -0600

    CheckStyle warning corrected

--- src/test/groovy/com/word/play/init/WordReaderTest.java
+++ src/test/groovy/com/word/play/init/WordReaderTest.java
@@ -1,60 +1,76 @@
 package com.word.play.init;
 
-import com.sanbox.junit.rules.Log4JMDCPopulatorRule;
+import com.sanbox.junit.rules.Log4jMdcPopulatorRule;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Rule;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Map;
 
+import static org.junit.Assert.assertTrue;
+
+/**
+ * This is Test class for WorReader
+ *
+ * @author Ravi
+ * @version 1.0
+ * @see WordReader
+ */
 public class WordReaderTest {
 
-    @Rule
-    public Log4JMDCPopulatorRule log4JMDCPopulatorRule = new Log4JMDCPopulatorRule();
+  @Rule
+  public Log4jMdcPopulatorRule log4JMdcPopulatorRule = new Log4jMdcPopulatorRule();
 
-    private static final Logger logger = LogManager.getLogger();
+  private static final Logger logger = LogManager.getLogger();
 
-    // TC01 - Test the loading of resources
-    @Test
-    public void testLoadFiles() {
-        logger.entry();
-        Map<String, Integer> map = WordReader.execute("testdata/wordreader/tc01");
-        assertTrue("Map size should be greater than zero", map.size()>0);
-        logger.exit();
-    }
+  // TC01 - Test the loading of resources
 
-    //TC02 - Predetermined text as below
-    //Brown fox jumped on green Green 'gREEn' Fox
-    //          Greener than "GreEn"
-    // brown occurs 1 time
-    // fox occurs 2 times
-    // jumped occurs 1 time
-    // on occurs 1 time
-    // green occurs 4 times
-    // greener occurs 1 time
-    // than occurs 1 time
-    @Test
-    public void testFrequencies() {
-        logger.entry();
-        Map<String, Integer> map = WordReader.execute("testdata/wordreader/tc02");
-        assertTrue("brown should occur once", map.get("brown")==1);
-        assertTrue("fox should occur 2 times", map.get("fox")==2);
-        assertTrue("jumped should occur once", map.get("jumped")==1);
-        assertTrue("on should occur once", map.get("on")==1);
-        assertTrue("green should occur 4 times", map.get("green")==4);
-        assertTrue("greener should occur once", map.get("greener")==1);
-        assertTrue("than should occur once", map.get("than")==1);
-        logger.exit();
-    }
+  /**
+   * TC01 - Test the loading of resources.
+   */
+  @Test
+  public void testLoadFiles() {
+    logger.entry();
+    Map<String, Integer> map = WordReader.execute("testdata/wordreader/tc01");
+    assertTrue("Map size should be greater than zero", map.size() > 0);
+    logger.exit();
+  }
 
-    // TC03 - Invoke WordReader with folder name
-    // that is not found in classpath.
-    @Test(expected = IllegalArgumentException.class)
-    public void testDirNotInClassPath() {
-        logger.entry();
-        WordReader.execute("no-folder");
-        logger.exit();
-    }
+   /**
+   * TC02 - Predetermined text as below.
+   * Brown fox jumped on green Green 'gREEn' Fox
+   *          Greener than "GreEn"
+   * brown occurs 1 time
+   * fox occurs 2 times
+   * jumped occurs 1 time
+   * on occurs 1 time
+   * green occurs 4 times
+   * greener occurs 1 time
+   * than occurs 1 time
+   */
+  @Test
+  public void testFrequencies() {
+    logger.entry();
+    Map<String, Integer> map = WordReader.execute("testdata/wordreader/tc02");
+    assertTrue("brown should occur once", map.get("brown") == 1);
+    assertTrue("fox should occur 2 times", map.get("fox") == 2);
+    assertTrue("jumped should occur once", map.get("jumped") == 1);
+    assertTrue("on should occur once", map.get("on") == 1);
+    assertTrue("green should occur 4 times", map.get("green") == 4);
+    assertTrue("greener should occur once", map.get("greener") == 1);
+    assertTrue("than should occur once", map.get("than") == 1);
+    logger.exit();
+  }
+
+  /**
+   * TC03 - Invoke WordReader with folder name
+   * that is not found in classpath.
+   */
+  @Test(expected = IllegalArgumentException.class)
+  public void testDirNotInClassPath() {
+    logger.entry();
+    WordReader.execute("no-folder");
+    logger.exit();
+  }
 }
--- src/test/java/com/sanbox/junit/rules/Log4JMDCPopulatorRule.java
+++ src/test/java/com/sanbox/junit/rules/Log4JMDCPopulatorRule.java
@@ -4,17 +4,27 @@ import org.apache.logging.log4j.ThreadContext;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 
-public class Log4JMDCPopulatorRule extends TestWatcher {
-    @Override
-    protected void starting(Description description) {
-        super.starting(description);
-        String displayName = description.getDisplayName();
-        ThreadContext.put("displayName", displayName);
-    }
+/**
+ * This is Junit rule for populating Log4J MDC
+ * Context so that when logging happens for tests
+ * we know to which class logs belong to.
+ *
+ * @author Ravi
+ * @see org.junit.rules.TestRule
+ * @see org.junit.rules.TestWatcher
+ *
+ */
+public class Log4jMdcPopulatorRule extends TestWatcher {
+  @Override
+  protected void starting(Description description) {
+    super.starting(description);
+    String displayName = description.getDisplayName();
+    ThreadContext.put("displayName", displayName);
+  }
 
-    @Override
-    protected void finished(Description description) {
-        super.finished(description);
-        ThreadContext.clearAll();
-    }
+  @Override
+  protected void finished(Description description) {
+    super.finished(description);
+    ThreadContext.clearAll();
+  }
 }
